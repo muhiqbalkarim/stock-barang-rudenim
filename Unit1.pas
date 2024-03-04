349b9798f9849ab49c07d1c63b180d4076569447
dirmansyahr <dirmansyahmuhammad134@gmail.com>
2024-03-04 15:05:38 +0800

    menambahkan groupbox1

--- Unit1.pas
+++ Unit1.pas
@@ -5,11 +5,14 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
-  FMX.Controls.Presentation, FMX.StdCtrls;
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Edit;
 
 type
   TForm1 = class(TForm)
     Label1: TLabel;
+    GroupBox1: TGroupBox;
+    Label2: TLabel;
+    Edit1: TEdit;
   private
     { Private declarations }
   public
