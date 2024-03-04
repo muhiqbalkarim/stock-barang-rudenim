6a5c0799b899fcdc0438ac97e870c3e166b86242
dirmansyahr <dirmansyahmuhammad134@gmail.com>
2024-03-04 22:48:31 +0800

    menambah tampilan form ambil barang

--- Unit1.pas
+++ Unit1.pas
@@ -5,14 +5,27 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
-  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Edit;
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Edit, System.Rtti,
+  FMX.Grid.Style, FMX.ScrollBox, FMX.Grid;
 
 type
   TForm1 = class(TForm)
     Label1: TLabel;
-    GroupBox1: TGroupBox;
     Label2: TLabel;
-    Edit1: TEdit;
+    Label3: TLabel;
+    Label4: TLabel;
+    GroupBox1: TGroupBox;
+    Label5: TLabel;
+    kodebarang: TEdit;
+    namabarang: TEdit;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    jumlahambil: TEdit;
+    penerima: TEdit;
+    Grid1: TGrid;
+    Button1: TButton;
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -25,5 +38,11 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  label3.Text:=formatdatetime('dddd, dd mmmm yyyy',date());
+  label4.Text:=formatdatetime('hh:mm',now());
+end;
 
 end.
